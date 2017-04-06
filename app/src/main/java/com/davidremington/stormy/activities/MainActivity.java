463b94d8b97cf11230703baae16ad1d567e8c99d
David Remington <david.ian.remington@gmail.com>
2017-04-06 10:01:37 -0700

    add methods to connect ForcastService

--- app/src/main/java/com/davidremington/stormy/activities/MainActivity.java
+++ app/src/main/java/com/davidremington/stormy/activities/MainActivity.java
@@ -1,15 +1,16 @@
-package com.davidremington.stormy.stormy.activities;
+package com.davidremington.stormy.activities;
 
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.davidremington.stormy.stormy.services.ForecastService;
-import com.davidremington.stormy.stormy.utils.NullForecastError;
+import com.davidremington.stormy.services.ForecastService;
+import com.davidremington.stormy.utils.NullForecastError;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 
@@ -17,17 +18,18 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.List;
 
-import butterknife.BindView;
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     private static Gson gson = new Gson();
     private static ForecastService forecastService;
 
-    @BindView(R.id.getLocationButton) Button locationButton;
-    @BindView(R.id.locationEditText) EditText locationText;
-    @BindView(R.id.errorTextView) TextView errorTextView;
+    @Bind(R.id.getLocationButton) Button locationButton;
+    @Bind(R.id.locationEditText) EditText locationText;
+    @Bind(R.id.errorTextView) TextView errorTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,28 +39,29 @@ public class MainActivity extends AppCompatActivity {
         forecastService = ForecastService.getInstance();
     }
 
-    public void getForecast() {
+    @OnClick(R.id.getLocationButton)
+    public void getForecast(View view) {
         LatLng coordinates = getLocationCoordinates();
         try {
             forecastService.getForecast(coordinates.latitude, coordinates.longitude);
         } catch (NullForecastError e) {
-
+            errorTextView.setText(getString(R.string.no_location_found));
         }
     }
 
     private LatLng getLocationCoordinates() {
         Geocoder coder = new Geocoder(this);
         List<Address> addresses;
-        LatLng point = null;
+        LatLng point;
 
         try {
-            addresses = coder.getFromLocationName(locationText.getText().toString(), 1);
+            addresses = coder.getFromLocationName(locationText.getText().toString(), 5);
             if(addresses == null) {
                 return null;
             }
             Address location = addresses.get(0);
-            point = new LatLng((double) (location.getLatitude()),
-                              ((double) (location.getLongitude())));
+            point = new LatLng(location.getLatitude(),
+                              (location.getLongitude()));
         } catch (IOException e) {
             return null;
         }
