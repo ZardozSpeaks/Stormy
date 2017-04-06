417281114e791a5e50a31f6b16f45fb0a0c21b0c
David Remington <david.ian.remington@gmail.com>
2017-04-06 09:57:32 -0700

    remove unused files

--- app/src/androidTest/java/com/davidremington/stormy/stormy/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/davidremington/stormy/stormy/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.davidremington.stormy;
+package com.davidremington.stormy.stormy;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.davidremington.stormy", appContext.getPackageName());
+        assertEquals("com.davidremington.stormy.stormy", appContext.getPackageName());
     }
 }
