8e5d2a6d99c3d19f487855be8540fad4cbce4142
David Remington <david.ian.remington@gmail.com>
2017-04-25 16:07:38 -0700

    add utility method for retreiving icons by name

--- app/src/main/java/com/davidremington/stormy/utils/Utils.java
+++ app/src/main/java/com/davidremington/stormy/utils/Utils.java
@@ -1,10 +1,14 @@
 package com.davidremington.stormy.utils;
 
 
+import com.davidremington.stormy.activities.R;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+
+import timber.log.Timber;
 
 public class Utils {
 
@@ -12,5 +16,46 @@ public class Utils {
         SimpleDateFormat formatter = new SimpleDateFormat("h:mm a", Locale.US);
         formatter.setTimeZone(TimeZone.getTimeZone(timezone));
         return formatter.format(new Date(timestamp * 1000));
+    }
+
+    public static int getIconId(String iconName) {
+        int iconId;
+        switch(iconName) {
+            case "clear-day":
+                iconId = R.mipmap.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.mipmap.clear_night;
+                break;
+            case "rain":
+                iconId = R.mipmap.rain;
+                break;
+            case "snow":
+                iconId = R.mipmap.snow;
+                break;
+            case "sleet":
+                iconId = R.mipmap.sleet;
+                break;
+            case "wind":
+                iconId = R.mipmap.wind;
+                break;
+            case "fog":
+                iconId = R.mipmap.fog;
+                break;
+            case "cloudy":
+                iconId = R.mipmap.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.mipmap.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.mipmap.cloudy_night;
+                break;
+            default:
+                iconId = 0;
+                Timber.e(String.format("unknown iconName %s passed to method", iconName));
+                break;
+        }
+        return iconId;
     }
 }
