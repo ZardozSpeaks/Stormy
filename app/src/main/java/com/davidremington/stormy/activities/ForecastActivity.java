5ff9fc8062c29d02776b54c53264a1531d61050c
David Remington <david.ian.remington@gmail.com>
2017-08-02 14:32:12 -0700

    improved model mapping for Gson

--- app/src/main/java/com/davidremington/stormy/activities/ForecastActivity.java
+++ app/src/main/java/com/davidremington/stormy/activities/ForecastActivity.java
@@ -1,12 +1,10 @@
 package com.davidremington.stormy.activities;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.davidremington.stormy.models.Forecast;
-import com.davidremington.stormy.utils.ApplicationContextProvider;
 import com.google.gson.Gson;
 
 import butterknife.ButterKnife;
@@ -14,16 +12,15 @@ import butterknife.ButterKnife;
 public class ForecastActivity extends AppCompatActivity {
 
     private Forecast mForecast;
-    private static Gson sGson;
+    private static Gson sGson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Context context = getApplicationContext();
-        ApplicationContextProvider.setContext(context);
         Bundle extras = getIntent().getExtras();
+        String forecastJSON = extras.getString("forecastData");
         setContentView(R.layout.activity_forecast);
         ButterKnife.bind(this);
-        mForecast = sGson.fromJson(extras.getString("forecastData"), Forecast.class);
+        mForecast = sGson.fromJson(forecastJSON, Forecast.class);
     }
 }
--- app/src/main/java/com/davidremington/stormy/models/Hourly.java
+++ app/src/main/java/com/davidremington/stormy/models/Hourly.java
@@ -6,6 +6,7 @@ class Hourly {
     public String summary;
     public String icon;
     public Integer nearestStormDistance;
+    public Integer nearestStormBearing;
     public Double precipIntensity;
     public Double precipIntensityError;
     public Double precipProbability;
@@ -15,10 +16,12 @@ class Hourly {
     public Double dewPoint;
     public Double humidity;
     public Double windSpeed;
+    public Double windGust;
     public Integer windBearing;
     public Double visablity;
     public Double cloudCover;
     public Double pressure;
     public Double ozone;
+    public Integer uvIndex;
     public Minutely minutely;
 }
